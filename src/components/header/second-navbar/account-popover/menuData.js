b12099852581f68894e1ec0f52d6021c33def59d
manish-ozi <manish.kumar.cn@ozi.in>
2025-07-28 15:31:24 +0530

    Menu bar update

--- src/components/header/second-navbar/account-popover/menuData.js
+++ src/components/header/second-navbar/account-popover/menuData.js
@@ -22,52 +22,52 @@ export const menuData = [
     path: "/my-orders",
   },
   {
-    id: 3,
-    name: "my-trips",
-    icon: <LocalTaxiIcon />,
-    path: "/my-trips",
-  },
-  {
-    id: 4,
-    name: "wallet",
-    icon: <WalletIcon />,
-    path: "/wallet",
-  },
-  {
-    id: 5,
-    name: "coupons",
-    icon: <ConfirmationNumberIcon />,
-    path: "/coupons",
-  },
-
-  {
-    id: 6,
-    name: "loyalty-points",
-    icon: <LoyaltyIcon />,
-    path: "/loyalty-points",
-  },
-  {
-    id: 7,
-    name: "referral-code",
-    icon: <SendToMobileIcon />,
-    path: "/referral-code",
-  },
-  {
-    id: 8,
-    name: "inbox",
-    icon: <SettingsIcon />,
-    path: "/settings",
-  },
-  {
-    id: 9,
-    name: "settings",
-    icon: <SettingsIcon />,
-    path: "/settings",
-  },
-  {
     id: 10,
     name: "track-order",
     icon: <LocalShippingOutlinedIcon />,
     path: "/track-order",
   },
+  // {
+  //   id: 3,
+  //   name: "my-trips",
+  //   icon: <LocalTaxiIcon />,
+  //   path: "/my-trips",
+  // },
+  // {
+  //   id: 4,
+  //   name: "wallet",
+  //   icon: <WalletIcon />,
+  //   path: "/wallet",
+  // },
+  // {
+  //   id: 5,
+  //   name: "coupons",
+  //   icon: <ConfirmationNumberIcon />,
+  //   path: "/coupons",
+  // },
+
+  // {
+  //   id: 6,
+  //   name: "loyalty-points",
+  //   icon: <LoyaltyIcon />,
+  //   path: "/loyalty-points",
+  // },
+  // {
+  //   id: 7,
+  //   name: "referral-code",
+  //   icon: <SendToMobileIcon />,
+  //   path: "/referral-code",
+  // },
+  // {
+  //   id: 8,
+  //   name: "inbox",
+  //   icon: <SettingsIcon />,
+  //   path: "/settings",
+  // },
+  // {
+  //   id: 9,
+  //   name: "settings",
+  //   icon: <SettingsIcon />,
+  //   path: "/settings",
+  // },
 ];
